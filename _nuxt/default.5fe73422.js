bdebab2f332ef6d7cb8fa9fb640b6918e979cc42
mikey <mforest@protonmail.com>
2023-05-06 18:56:53 -0400

    Updated baseurl

---
+++ _nuxt/default.5fe73422.js
@@ -0,0 +1,1 @@
+import{a as t,o as a,e as o,m as n}from"./entry.0a3691ee.js";const c=t({__name:"default",setup(r){return(e,s)=>(a(),o("div",null,[n(e.$slots,"default")]))}});export{c as default};
--- _payload.js
+++ _payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1683413277965}
+export default {data:{},prerenderedAt:1683413784724}
